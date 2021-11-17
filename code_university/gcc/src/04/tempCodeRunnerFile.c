babbe2e6e7772c7b4382d779ca5235a2d06eba42
nisshimura <t-nishimura.temp@sophia-s.co.jp>
2021-11-18 02:33:34 +0900

    orgnized to master

--- code_university/gcc/src/04/tempCodeRunnerFile.c
+++ code_university/gcc/src/04/tempCodeRunnerFile.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-int aster(int x)
-{  
-    int i;
-
-    for (i=0;i<x;i++) {
-        printf("*");
-    }
-    return 0;
-}
-int main()
-{   
-    int z;
-    printf("input a positive integer:");
-    scanf("%d", &z);
-    int y = aster(z);
-}
+    // char c = c + '0';
