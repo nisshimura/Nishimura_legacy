4ff844515faffed8948d1c7b3a8af4509efa4c5e
nisshimura <t-nishimura.temp@sophia-s.co.jp>
2021-12-22 04:05:25 +0900

    add 10

--- code_university/prokiso2/10/10-b-03.c
+++ code_university/prokiso2/10/10-b-03.c
@@ -6,11 +6,18 @@
 int mystrcat(char *s1,char *s2)
 {
     int i=0,len;
-    len = strlen(s1)-1;
-    printf("%d",len+i);
+    len = strlen(s1);
+    printf("%d",len);
     while (s2[i]!='\0')
     {
-        s1[len+i]=s2[i];
+        if (s2[i]=='\n')
+        {
+            ;
+        }
+        else
+        {
+            s1[len + i] = s2[i];
+        }
         i++;
     }
     return 0;
@@ -26,10 +33,14 @@ void strrpt(char *s1,char *s2,int n)
 int main()
 {
     char s1[MAX_LEN], s2[MAX_LEN];
-    int n;
+    int n,len;
     printf("Input string : ");
-    fgets(s1,MAX_LEN,stdin);    
-    s1[strlen(s1)-1]='\0';
+    fgets(s1,MAX_LEN,stdin);
+    len = strlen(s1);
+    printf("%d", len);
+    s1[len-1] = '\0';
+    len = strlen(s1);
+    printf("%d",len);
     printf("Input repeat time : ");
     scanf("%d",&n);
 
