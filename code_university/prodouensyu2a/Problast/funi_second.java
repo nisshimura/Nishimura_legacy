3ffdca3d0687de6571fad6efea40758783ebfe75
nisshimura <t-nishimura.temp@sophia-s.jp>
2022-07-18 16:56:44 +0900

    add funi_secon

--- code_university/prodouensyu2a/Problast/funi_second.java
+++ code_university/prodouensyu2a/Problast/funi_second.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class funi_second {
-    static final String[] ORDERS = {"ac", "af", "dc", "df", "sc", "sf", "return", "close"};
+    static final String[] ORDERS = { "ac", "af", "dc", "df", "sc", "sf", "ef","rf","return", "close" };
     static List<String> FUNITURES = new ArrayList<String>(Arrays.asList("chair", "table", "pot"));
 
     static List<List<Integer>> funis_youhave = new ArrayList<List<Integer>>();// FUNITURES
-    static int[][] tmp_num_funis = {{10, 0}, {9, 0}, {6, 0}};// remaining/rented
+    static int[][] tmp_num_funis = { { 10, 0 }, { 9, 0 }, { 6, 0 } };// remaining/rented
 
     static int nowID = 0;
     static List<List<String>> cus_funi_list = new ArrayList<List<String>>();// {{"Bob","0","chair","chair"},{"Tom","1","chair,"pot"}}
@@ -29,36 +29,53 @@ public class funi_second {
     }
 
     public boolean rental_funi(String name, String funi) {
-        if (!isInCusList(name)){
-            System.out.println("You are not member!\nPlz become member!");
-            return false;
-        }
-
+        int idx = 0;
         if (calc_num_funis(funi, true))
-            cus_funi_list.get(select_person(name)).add(funi);
+            idx = select_person(name);
+        cus_funi_list.get(idx).add(funi);
         return true;
     }
 
-    public boolean del_cus(String name) {
-        if (!isInCusList(name)) {
-            System.out.println("You are not member!");
-            return false;
-        }
+    public void return_funi(String name, String funi) {
         int index = select_person(name);
-        for (int i = 1; i < cus_funi_list.get(index).size(); i++)
+        cus_funi_list.get(index).remove(cus_funi_list.get(index).indexOf(funi));
+        calc_num_funis(funi, false);
+    }
+
+    public boolean del_cus(String name) {
+        System.out.println("del_cus");
+        int index = select_person(name);
+        for (int i = 2; i < cus_funi_list.get(index).size(); i++)
             calc_num_funis(cus_funi_list.get(index).get(i), false);
         cus_funi_list.remove(index);
         return true;
     }
 
-    public int count_funi(int id,String funi) {
+    public int count_funi(String id, String funi) {
         int count = 0;
-        for (int i = 0; i < cus_funi_list.get(indexOfCusList(name)).size(); i++)
-            if (cus_funi_list.get(indexOfCusList(name)).get(i).equals(funi))
+        for (int i = 0; i < cus_funi_list.get(indexOfCusList_id(id)).size(); i++)
+            if (cus_funi_list.get(indexOfCusList_id(id)).get(i).equals(funi))
                 count++;
         return count;
     }
 
+    public boolean entry_funi(String funi){
+        if (!isAvailableFuni(funi)){
+            FUNITURES.add(funi);
+            return true;
+        }
+        System.out.println("This is already in funis_list");
+        System.out.println(FUNITURES);
+        return false;
+    }
+
+    public boolean remove_funi(String funi){
+        if (!isAvailableFuni(funi)){
+            FUNITURES.remove(indexOfFuniList(funi));
+            return true;
+        }
+        return false;
+    }
     public boolean calc_num_funis(String funi, boolean isRental) {
         if (!isYouHaveStock(funi))
             return false;
@@ -76,39 +93,63 @@ public class funi_second {
         return true;
     }
 
-    public int select_person(String name){
+    public int select_person(String name) {
         List<Integer> p = indexOfCusList(name);
         String str;
         if (p.size() == 1)
-            return 0;
+            return p.get(0);
         else {
             System.out.println(name + " is used by " + Integer.valueOf(p.size()) + " people");
-            print_sf();
-            System.out.print("Plz select index 0 to "+Integer.valueOf(p.size())+" : ");
+            print_by_name(name);
+            System.out.print("Plz select index 0 to " + Integer.valueOf(p.size() - 1) + " : ");
             Scanner scan = new Scanner(System.in);
             str = scan.nextLine();
         }
-        return p.get(Integer.valueOf(str));
-    }
-    public void print_sf(){
-        String formatter1 = "%7s%-15s%-15s%n";
-        System.out.printf(formatter1, "index","funi_name", "remain/rented");
-        for (int i = 0; i < funis_youhave.size(); i++)
-            System.out.printf(formatter1, Integer.valueOf(i),FUNITURES.get(i),String.valueOf(funis_youhave.get(i).get(0)) + "/"+ String.valueOf(funis_youhave.get(i).get(1)));
+        int idx = p.get(Integer.valueOf(str));
+        return idx;
     }
 
-    public void print_sc(){
-        System.out.printf("%7s%-15s","index","name");
+    public void print_sf() {
+        String formatter1 = "%-7s%-15s%-15s%-n";
+        System.out.printf(formatter1, "index", "funi_name", "remain/rented");
+        for (int i = 0; i < funis_youhave.size(); i++)
+            System.out.printf(formatter1, Integer.valueOf(i), FUNITURES.get(i),
+                    String.valueOf(funis_youhave.get(i).get(0)) + "/" + String.valueOf(funis_youhave.get(i).get(1)));
+    }
+
+    public void print_by_name(String name) {
+        List<Integer> p = indexOfCusList(name);
+
+        System.out.printf("%-7s%-15s%-7s", "index", "name", "id");
         for (int i = 0; i < FUNITURES.size(); i++)
-            System.out.printf("%10s", FUNITURES.get(i));
+            System.out.printf("%-10s", FUNITURES.get(i));
+        System.out.printf("%n");
+
+        for (int i = 0; i < p.size(); i++) {
+            String id = cus_funi_list.get(i).get(1);
+            System.out.printf("%-7s", i);
+            System.out.printf("%-15s", name);
+            System.out.printf("%-5s", id);
+            for (int j = 0; j < FUNITURES.size(); j++)
+                System.out.printf("%-10s", count_funi(cus_funi_list.get(i).get(1), FUNITURES.get(j)));
+            System.out.printf("%n");
+        }
+    }
+
+    public void print_sc() {
+        System.out.printf("%-7s%-15s%-7s", "index", "name", "id");
+        for (int i = 0; i < FUNITURES.size(); i++)
+            System.out.printf("%-10s", FUNITURES.get(i));
         System.out.printf("%n");
 
         for (int i = 0; i < cus_funi_list.size(); i++) {
             String name = cus_funi_list.get(i).get(0);
-            System.out.printf("%7s", i);
-            System.out.printf("%15s", name);
+            String id = cus_funi_list.get(i).get(1);
+            System.out.printf("%-7s", i);
+            System.out.printf("%-15s", name);
+            System.out.printf("%-5s", id);
             for (int j = 0; j < FUNITURES.size(); j++)
-                System.out.printf("%10s",count_funi(cus_funi_list.get(i).get(0), FUNITURES.get(j)));
+                System.out.printf("%-10s", count_funi(cus_funi_list.get(i).get(1), FUNITURES.get(j)));
             System.out.printf("%n");
         }
     }
@@ -120,12 +161,6 @@ public class funi_second {
         return flag;
     }
 
-    public void return_funi(String name, String funi) {
-        cus_funi_list.get(indexOfCusList(name))
-                .remove(cus_funi_list.get(indexOfCusList(name)).indexOf(funi));
-        calc_num_funis(funi, false);
-    }
-
     public List<Integer> indexOfCusList(String name) {
         List<Integer> num = new ArrayList<Integer>();
         for (int i = 0; i < cus_funi_list.size(); i++)
@@ -133,12 +168,19 @@ public class funi_second {
                 num.add(i);
         return num;
     }
-    
-    public int indexOfCusList_id(int id) {
+
+    public int indexOfFuniList(String funi) {
+        int num = 0;
+        for (int i = 0; i < FUNITURES.size(); i++)
+            if (FUNITURES.get(i).equals(funi))
+                num=i;
+        return num;
+    }
+    public int indexOfCusList_id(String id) {
         int num = 0;
         for (int i = 0; i < cus_funi_list.size(); i++)
-            if (cus_funi_list.get(i).get(0).equals(name))
-                num=i;
+            if (cus_funi_list.get(i).get(1).equals(id))
+                num = i;
         return num;
     }
 
@@ -147,6 +189,8 @@ public class funi_second {
         for (int i = 0; i < cus_funi_list.size(); i++)
             if (cus_funi_list.get(i).get(0).equals(name))
                 flag = true;
+        if (!flag)
+            System.out.println("You are not member!");
         return flag;
     }
 
@@ -172,95 +216,119 @@ public class funi_second {
 
     public static void main(String[] args) {
 
-
         funi_second a = new funi_second();
         funis_youhave = array2list(tmp_num_funis);
 
         for (;;) {
             Scanner scan = new Scanner(System.in);
-            System.out.println("Enter admin/customer");
+            System.out.println("Enter admin/customer end to \"close\"");
             String str = scan.nextLine();
-            if (!(str.equals("admin") || str.equals("customer")))
-                continue;
-            boolean isAdmin = str.equals("admin");
-            if (isAdmin) {// Administrator
-                for (;;) {
-                    System.out.println("Enter Your Order: ");
-                    System.out.println("ADD Customer : ac");
-                    System.out.println("ADD Furniture : af");
+            switch (str) {
+                case "admin":
+                    for (;;) {
+                        System.out.println("Enter Your Order: ");
+                        System.out.println("ADD Customer : ac");
+                        System.out.println("ADD Furniture : af");
 
-                    System.out.println("Delete Customer : dc");
-                    System.out.println("Delete Furniture : df");
+                        System.out.println("Delete Customer : dc");
+                        System.out.println("Delete Furniture : df");
 
-                    System.out.println("Show Num Of Funitures (remaining/rented): sf");
-                    System.out.println("Show ALL Customers Information: sc");
+                        System.out.println("Show Num Of Funitures (remaining/rented): sf");
+                        System.out.println("Show ALL Customers Information: sc");
 
-                    System.out.println("Return : return");
-                    System.out.println("Close : close");
+                        System.out.println("Entry funitures customer can rent : ef");
+                        System.out.println("Remove funitures customer can rent : rf");
 
-                    String order = scan.nextLine();
-                    if (!a.isAvailableOrder(order))
-                        continue;
-                    if (order.equals("return"))
+                        System.out.println("Return : return");
+                        System.out.println("Close : close");
+
+                        String order = scan.nextLine();
+                        if (order.equals("return"))
+                            break;
+                        switch (order) {
+                            case "ac":
+                                // ADD Customer
+                                System.out.println(
+                                        "Enter New Customers you need to add (Plz split with ,)");
+                                String name1 = scan.nextLine();
+                                String[] names1 = name1.split(",");
+                                for (int i = 0; i < names1.length; i++)
+                                    a.add_cus(names1[i]);
+                                break;
+                            case "af":
+                                // ADD Funiture
+                                System.out.println("Enter Target Customer you need to add");
+                                String name2 = scan.nextLine();
+                                if (!a.isInCusList(name2))
+                                    break;
+                                System.out.println("Enter Funitures you need rent (Plz split with ,)");
+                                String funi2 = scan.nextLine();
+                                String[] funis2 = funi2.split(",");
+                                for (int i = 0; i < funis2.length; i++)
+                                    if (a.isAvailableFuni(funis2[i]))
+                                        a.rental_funi(name2, funis2[i]);
+                                break;
+                            case "dc":
+                                // DELETE Customers
+                                System.out.println(
+                                        "Enter customers you need to DELETE (Plz split with ,)");
+                                String name3 = scan.nextLine();
+                                String[] names3 = name3.split(",");
+                                System.out.println(Arrays.toString(names3));
+                                for (int i = 0; i < names3.length; i++)
+                                    if (a.isInCusList(names3[i])) {
+                                        a.del_cus(names3[i]);
+                                    }
+
+                                break;
+                            case "df":
+                                // DELETE Funitures
+                                System.out.println("Enter Target Customer you need to DELETE");
+                                String name4 = scan.nextLine();
+                                System.out.println("Enter Funitures you need DELETE (Plz split with ,)");
+                                String funi4 = scan.nextLine();
+                                String[] funis4 = funi4.split(",");
+                                for (int i = 0; i < funis4.length; i++)
+                                    if (a.isAvailableFuni(funis4[i]))
+                                        a.return_funi(name4, funis4[i]);
+                                break;
+                            case "sf":
+                                a.print_sf();
+                                break;
+                            case "sc":
+                                a.print_sc();
+                                break;
+                            case "ef":
+                                System.out.println("Enter Funis you ADD funis_list (pls split with ,)");
+                                String funi7 = scan.nextLine();
+                                String[] funis7 = funi7.split(",");
+                                for (int i = 0; i < funis7.length; i++)
+                                        a.entry_funi(funis7[i]);
+                                break;
+                            case "rf":
+                                System.out.println("Enter Funis you REMOVE funis_list (pls split with ,)");
+                                String funi8 = scan.nextLine();
+                                String[] funis8 = funi8.split(",");
+                                for (int i = 0; i < funis8.length; i++)
+                                        a.remove_funi(funis8[i]);
+                                break;  
+                            case "close":
+                                System.out.println("See you!!");
+                                System.exit(1);
+                            default:
+                                System.out.println(order + ", This Order is not AVAILABLE");
+                                break;
+                        }
+                        System.out.println(cus_funi_list);
                         break;
-                    switch (order) {
-                        case "ac":
-                            // ADD Customer
-                            System.out.println(
-                                    "Enter New Customers you need to add (Plz split with ,)");
-                            String name1 = scan.nextLine();
-                            String[] names1 = name1.split(",");
-                            for (int i = 0; i < names1.length; i++)
-                                a.add_cus(names1[i]);
-                            break;
-                        case "af":
-                            // ADD Funiture
-                            System.out.println(
-                                    "Enter Target Customer you need to add (OK to new one)");
-                            String name2 = scan.nextLine();
-                            System.out.println("Enter Funitures you need rent (Plz split with ,)");
-                            String funi2 = scan.nextLine();
-                            String[] funis2 = funi2.split(",");
-                            for (int i = 0; i < funis2.length; i++)
-                                if (a.isAvailableFuni(funis2[i]))
-                                    a.rental_funi(name2, funis2[i]);
-                            break;
-                        case "dc":
-                            // DELETE Customers
-                            System.out.println(
-                                    "Enter customers you need to DELETE (Plz split with ,)");
-                            String name3 = scan.nextLine();
-                            String[] names3 = name3.split(",");
-                            for (int i = 0; i < names3.length; i++)
-                                a.del_cus(names3[i]);
-                            break;
-                        case "df":
-                            // DELETE Funitures
-                            System.out.println("Enter Target Customer you need to DELETE");
-                            String name4 = scan.nextLine();
-                            System.out.println("Enter Funitures you need DELETE (Plz split with ,)");
-                            String funi4 = scan.nextLine();
-                            String[] funis4 = funi4.split(",");
-                            for (int i = 0; i < funis4.length; i++)
-                                if (a.isAvailableFuni(funis4[i]))
-                                    a.return_funi(name4, funis4[i]);
-                            break;
-                        case "sf":
-                            a.print_sf();
-                            break;
-                        case "sc":
-                            a.print_sc();
-                            break;
-                        case "close":
-                            System.out.println("See you!!");
-                            System.exit(1);
                     }
-                }
-            } else {
-                System.out.println("Enter new/member");
-
+                case "close":
+                    System.out.println("See you!!");
+                    System.exit(1);
+                default:
+                    System.out.println("pls select in admin/customer");
+                    break;
             }
-            System.out.println(cus_funi_list);
         }
     }
 }
